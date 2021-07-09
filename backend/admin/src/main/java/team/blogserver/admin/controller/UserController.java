7af8f156729956bd73cdbf644847171c118488f8
lshzz <528118879@qq.com>
2021-07-09 07:27:20 +0000

    !6 用户管理1.1 * 用户管理 * Merge branch 'master' of gitee.com:Ashinch/vblog into lshz * 添加用户 * Merge branch 'master' of gitee.com:Ashinch/vblog into lshz * 用户控制器 * 用户管理模块

--- backend/admin/src/main/java/team/blogserver/admin/controller/UserController.java
+++ backend/admin/src/main/java/team/blogserver/admin/controller/UserController.java
@@ -23,7 +23,14 @@ public class UserController {
     @Resource
     private UserService userService;
 
-    @ApiOperation("用户列表")
+    /**
+     * 查询用户列表
+     * @param page 当前页
+     * @param limit 每页显示数
+     * @param name  输入用户名或者昵称查找
+     * @return 用户列表
+     */
+    @ApiOperation(value = "用户列表", notes = "传name既是根据用户名或者昵称模糊查找，为空则查全部")
     @GetMapping("/list/{page}/{limit}/{name}")
     public R list(@PathVariable Long page, @PathVariable Long limit, String name) {
         Page<User> pageParam = new Page<>(page, limit);
@@ -31,6 +38,10 @@ public class UserController {
         return R.ok(listPage);
     }
 
+    /**
+     * 删除用户
+     * @param id  用户ID
+     */
     @ApiOperation("删除用户")
     @DeleteMapping("/remove/{id}")
     public R removeById(@PathVariable Long id) {
@@ -41,7 +52,11 @@ public class UserController {
         return R.error("删除失败");
     }
 
-    @ApiOperation("禁用用户")
+    /**
+     * 禁用或启用用户
+     * @param id 用户id
+     */
+    @ApiOperation("禁用或启用用户")
     @PutMapping("/update/{id}")
     public R updateById(@PathVariable Long id) {
         boolean result = userService.updateById(id);
@@ -49,5 +64,30 @@ public class UserController {
             return R.ok(null);
         }
         return R.error("禁用失败");
+    }
+
+    /**
+     * 新增用户
+     * @param user 用户实体类
+     */
+    @ApiOperation("新增用户")
+    @PostMapping("/save")
+    public R save(@RequestBody User user){
+        boolean result = userService.save(user);
+        if(result){
+            return R.ok(null);
+        }
+        return R.error("新增失败");
+    }
+
+    /**
+     * 更新用户
+     *
+     * @param user 用户实体类
+     */
+    @ApiOperation("更新用户")
+    @PutMapping("/update")
+    public R update(@RequestBody User user) {
+        return R.judge(userService.updateById(user));
     }
 }
